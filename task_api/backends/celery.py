34ecc3c9a89261175af4f657043c552daec43a6d
Jens Timmerman <github@caret.be>
2021-10-01 21:56:06 +0200

    task -> shared_task
    fixes #20

--- task_api/backends/celery.py
+++ task_api/backends/celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import json
 
-from celery import task
+from celery import shared_task
 
 from task_api.backends.base import TaskBackend
 from task_api.models import TaskInfo
@@ -14,7 +14,7 @@ class CeleryBackend(TaskBackend):
         info.backend_data = json.dumps({'celery_id': result.id})
 
 
-@task
+@shared_task
 def execute(task_id, class_str):
     info = TaskInfo.objects.get(pk=task_id)
     cls = CeleryBackend().resolve_class(class_str)
